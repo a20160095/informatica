8e19372fffd6eb3ad4650953fe4921237fcaab1f
a20160095 <a20160095@juan23.edu.pe>
2024-11-05 18:53:52 -0500

    Add files via upload

---
+++ app01.js
@@ -0,0 +1,5 @@
+document.write("<h1>bienvenidos a la pagina Java script</h1>");
+var nom = prompt("ingrese su nombre");
+document.write(
+  "hola" + nom + " es tu primer contacto con la programacion Javascript"
+);
